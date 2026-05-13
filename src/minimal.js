0c603fc9bbcff010d0b9728d74b6f67ae682f3ee
Didi Lusa <didi.lusak@gmail.com>
2026-05-13 20:25:33 +0200

    modification du 13 mai
    Modification de la page index, ajout des pages portfolio, atelier de géné, animation

--- src/minimal.js
+++ src/minimal.js
@@ -1,6 +1,11 @@
 const menuBtn = document.getElementById('menu-btn');
 const nav = document.getElementById('nav');
+const track = document.querySelector('.portfolio-content');
 
 menuBtn.addEventListener('click', () => {
   nav.classList.toggle('open');
 });
+
+/*----CAROUSSEL ARROWS----*/ 
+document.querySelector('.next').onclick = () => track.scrollBy({ left: 350, behavior: 'smooth' });
+document.querySelector('.prev').onclick = () => track.scrollBy({ left: -350, behavior: 'smooth' });
